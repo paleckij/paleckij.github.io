76b0f28a9b22eec7ed0bcb0c43232f5b9fc733f6
paleckij <paleckij.1988@gmail.com>
2017-04-01 21:57:57 +0300

    delete CV

--- CV/js/loader.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-      $(".polosa-zagruzki").each(function() {
-         $(this)
-         .data("origWidth", $(this).width())
-         .width(0)
-         .animate({width: $(this).data("origWidth")}, 1200);
-     });
-  });
